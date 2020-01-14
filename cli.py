58819e6bd4aafa4cb7e8916742602f07de0c5b93
Anthony Magner <anthony.magner@gmail.com>
2020-01-13 21:49:30 -0700

    adding stuffs

---
+++ cli.py
@@ -0,0 +1,9 @@
+import argparse
+import logging
+import os
+import sys
+from os import path, sep, getcwd, access, W_OK
+from pathlib import Path
+from platform import system
+
+log = logging.getLogger(__name__)
---
+++ config.py
@@ -0,0 +1,7 @@
+import logging
+
+BOT_DATA_DIR = '/home/antman/projects/chatbot/data'
+
+BOT_LOG_FILE = BOT_DATA_DIR + '/chatbot.log'
+
+BOT_LOG_LEVEL = logging.INFO
