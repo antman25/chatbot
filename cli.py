02531d68b3477cc099f48d7d24566d0585fde278
Anthony Magner <anthony.magner@gmail.com>
2020-01-13 21:59:47 -0700

    cli.py

--- cli.py
+++ cli.py
@@ -7,3 +7,14 @@ from pathlib import Path
 from platform import system
 
 log = logging.getLogger(__name__)
+
+def main():
+    execution_dir = getcwd()
+
+    # By default insert the execution path (useful to be able to execute Errbot from
+    # the source tree directly without installing it.
+    sys.path.insert(0, execution_dir)
+    print ('Anthony')
+
+if __name__ == "__main__":
+    main()
